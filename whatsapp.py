1a14ff1924eb21975dba040785bd9a9facd7875d
singhchirag502 <72164256+singhchirag502@users.noreply.github.com>
2020-10-01 02:03:43 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -14,3 +14,7 @@ driver.get('https://web.whatsapp.com/')
 time.sleep(10)
 driver.find_element_by_xpath('//*[@id="pane-side"]/div[1]/div/div/div[1]').click()
 time.sleep(1)
+for i in range(1,20):
+    keyboard.type("Good night")
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
