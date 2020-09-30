a76c25484bdf46322c0ff069c82f5511063e0e9f
chiragsingh1711 <62589134+chiragsingh1711@users.noreply.github.com>
2020-10-01 01:49:25 +0530

    Create whatsapp.py

---
+++ whatsapp.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import random
+from pynput.keyboard import Key, Controller
+
+
+chromedriver="F:\Python 3.8.3\chromedriver_win32\chromedriver"
+driver=webdriver.Chrome(chromedriver)
+keyboard=Controller()
+
+driver.get('https://web.whatsapp.com/')
+time.sleep(10)
+driver.find_element_by_xpath('//*[@id="pane-side"]/div[1]/div/div/div[1]').click()
+time.sleep(1)
